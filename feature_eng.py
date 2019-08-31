6e552837a57a8c4796602bd807a433b2ee0be6d7
Jungho Cho <junghocho@Junghoui-MacBook-Pro.local>
2019-08-30 20:34:13 -0700

    500 sample features with feature selection code

---
+++ feature_eng.py
@@ -0,0 +1,26 @@
+import random
+
+new_f = open('new_faa.txt', 'w')
+f = open('feature_answer_all.txt')
+
+n_t = 0
+n_f = 0
+feats = []
+for l in f:
+    if n_t >= 250 and n_f >= 250:
+        break
+    parts = l.strip().split()
+    target = int(float(parts[-1]))
+    if target == 1:
+        if n_t < 250:
+            feats.append(l)
+            n_t += 1
+    if target == 0:
+        if n_f < 250:
+            feats.append(l)
+            n_f += 1
+
+random.shuffle(feats)
+
+for l in feats:
+    new_f.write(l)
