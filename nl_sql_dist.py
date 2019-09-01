54a0b5ce210448892330233c020e48a480d1100b
JHCho-DBLab-Postech <jhcho@dblab.postech.ac.kr>
2019-09-01 16:32:13 -0700

    Minor error fix

--- nl_sql_dist.py
+++ nl_sql_dist.py
@@ -53,7 +53,7 @@ def draw_roc_curve(pred, target):
     fpr, tpr, threshold = roc_curve(target, pred)
     roc_auc = auc(fpr, tpr)
     plt.figure()
-    plt.plot(fpr[2], tpr[2], color='darkorange',
+    plt.plot(fpr, tpr, color='darkorange',
              lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
     plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
     plt.xlim([0.0, 1.0])
